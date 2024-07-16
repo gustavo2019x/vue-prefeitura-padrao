7da16cdc7b4ee4086fb06d9cd42eb0febfde4cfb
Gustavo Neri <gnb.neri@gmail.com>
2024-07-16 00:29:14 -0300

    test332e

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,29 @@
-import { defineComponent as l, ref as s, openBlock as u, createElementBlock as d, createElementVNode as e, toDisplayString as o, renderSlot as i } from "vue";
+import { defineComponent as l, ref as n, openBlock as s, createElementBlock as u, createElementVNode as e, toDisplayString as o, renderSlot as d } from "vue";
 import { cnpj as c } from "cpf-cnpj-validator";
-const f = { class: "bg-blue-700 h-10 w-full text-lg flex items-center justify-between" }, p = { class: "bg-blue-700 h-10 w-full" }, m = /* @__PURE__ */ l({
+const f = { class: "bg-blue-700 h-10 w-full text-lg flex items-center justify-between" }, i = { class: "bg-blue-700 h-10 w-full" }, b = /* @__PURE__ */ l({
   __name: "HeaderPrefeitura",
   props: {
     textoHeader: { default: "Cabeçalho Padrão Pref" },
     textoFooter: { default: "Rodapé Padrão" }
   },
-  setup(r) {
-    const a = s("Gerar CNPJ");
-    function n() {
-      a.value = c.generate();
+  setup(p) {
+    const r = n("Gerar CNPJ");
+    function a() {
+      r.value = c.generate();
     }
-    return (t, _) => (u(), d("div", null, [
+    return (t, m) => (s(), u("div", null, [
       e("header", f, [
         e("div", null, o(t.textoHeader), 1),
         e("button", {
           class: "text-white h-8 bg-green-900 p-1 w-40",
-          onClick: n
-        }, o(a.value), 1)
+          onClick: a
+        }, o(r.value), 1)
       ]),
-      i(t.$slots, "default"),
-      e("footer", p, o(t.textoFooter), 1)
+      d(t.$slots, "default"),
+      e("footer", i, o(t.textoFooter), 1)
     ]));
   }
-}), g = {
-  install: (r) => {
-    r.component("HeaderPrefeitura", m);
-  }
-};
+});
 export {
-  m as HeaderPrefeitura,
-  g as default
+  b as HeaderPrefeitura
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './vue-prefeitura-padrao.css'
 import HeaderPrefeitura from './components/HeaderPrefeitura.vue'
-import type { App } from "vue";
+// import type { App } from "vue";
 
-export default {
-    install: (app: App) => {
-        app.component('HeaderPrefeitura', HeaderPrefeitura);
-    },
-};
+// export default {
+//     install: (app: App) => {
+//         app.component('HeaderPrefeitura', HeaderPrefeitura);
+//     },
+// };
 
 export { HeaderPrefeitura }
